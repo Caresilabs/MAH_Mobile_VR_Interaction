bfc7569ee5b9d05ce8ed853a0d121dc54dcd1a33
Patrik Nilsson <craftopia@live.com>
2017-02-05 21:18:20 +0100

    Moved Project: "Patte-test" to "VR_Sinulation_Enviroment"

---
+++ VR_Simulation_Environment/Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField]
+    private GvrReticlePointer pointer;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
