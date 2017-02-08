d0c80306192debf89d88f03ac85fb724b2c2662b
Simon Bothén <caresilabs@gmail.com>
2017-02-08 10:31:01 +0100

    basic game manager

---
+++ VR_Simulation_Environment/Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+using Assets.Scripts;
+using System.Collections.Generic;
+using System;
+
+public class GameManager : MonoBehaviour {
+
+    public enum LevelState
+    {
+        PRACTICE, COMPETITION
+    }
+
+    public LevelState State { get; private set; }
+
+    public IChallenge CurrentChallenge { get; private set; }
+
+    public AnalyticsSampler Analytics { get; private set; }
+
+    [SerializeField]
+    private List<GameObject> challenges;
+
+    private GvrReticlePointer pointer;
+
+    private int challengeIndex;
+
+    // Use this for initialization
+    void Start () {
+        this.Analytics = GetComponent<AnalyticsSampler>();
+
+        this.pointer = FindObjectOfType<GvrReticlePointer>();
+        this.challengeIndex = 0;
+
+        Begin();
+	}
+
+    private void Begin()
+    {
+        State = LevelState.COMPETITION;
+
+        CurrentChallenge.Start();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    public void NextPhase()
+    {
+        if (State == LevelState.PRACTICE)
+        {
+            CurrentChallenge.Stop();
+            State = LevelState.COMPETITION;
+
+            CurrentChallenge.Start();
+        }
+        else
+        {
+            CurrentChallenge.Stop();
+
+            ++challengeIndex;
+            if (challengeIndex >= challenges.Count)
+            {
+                // next scene
+            }
+            else
+            {
+                CurrentChallenge = challenges[challengeIndex].GetComponent<IChallenge>();
+                CurrentChallenge.Start();
+            }
+        }
+    }
+}
--- VR_Simulation_Environment/Assets/Scripts/IChallenge.cs
+++ VR_Simulation_Environment/Assets/Scripts/IChallenge.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Assets.Scripts
 {
-    interface IChallenge
+    public interface IChallenge
     {
         void Start();       //Start the challange
 
