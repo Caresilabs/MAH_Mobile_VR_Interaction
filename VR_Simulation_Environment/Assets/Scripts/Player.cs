2856419b52af0725e99c6f92ad2873b42dd36411
Patrik Nilsson <craftopia@live.com>
2017-02-08 11:46:03 +0100

    Updated VR mode, controller mode

---
+++ VR_Simulation_Environment/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    private bool            controllerEnabled;
+    private GameObject      camera;
+
+	void Start () {
+        camera = GetComponentInChildren<Camera>().gameObject;
+        SetVrMode();
+	}
+	
+	void Update () {
+        if(controllerEnabled)
+        {
+            //update camera by controller here
+            //test
+            if (Input.anyKey)
+            {
+                transform.Rotate(camera.transform.up, 0.2f);
+            }
+            //
+        }
+	}
+
+    public void SetVrMode()
+    {
+        GvrViewer.Instance.VRModeEnabled = true;
+        controllerEnabled = false;
+    }
+
+    public void SetControllerMode()
+    {
+        GvrViewer.Instance.VRModeEnabled = false;
+        controllerEnabled = true;
+    }
+}
