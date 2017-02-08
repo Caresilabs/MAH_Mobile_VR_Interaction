8de644ab0bf765f46ef3b84be17f273ad5846fe4
Patrik Nilsson <craftopia@live.com>
2017-02-08 10:54:39 +0100

    Target Range VR

---
+++ VR_Simulation_Environment/Assets/Challenge/Target Range/TargetChallenge.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts;
+
+using System.Collections.Generic;
+
+public class TargetChallenge : MonoBehaviour, IChallenge {
+
+    [SerializeField]
+    private List<BoxCollider>   SpawnBoxColliders;
+
+    [SerializeField]
+    private GameObject          Target;
+
+    private int                 currentChild;
+
+    private GameObject          currentTarget;
+
+
+    public void StartChallenge()
+    {
+        currentChild = 0;
+        spawnTarget();
+    }
+
+    public void StopChallenge()
+    {
+
+    }
+
+    private void spawnTarget()
+    {
+        if (currentChild == transform.childCount)
+            currentChild = 0;
+
+        BoxCollider bx = transform.GetChild(currentChild).GetComponent<BoxCollider>();
+        Vector3 posSpawn = bx.transform.position + new Vector3(
+            Random.Range(-bx.size.x / 2, bx.size.x / 2),
+            Random.Range(-bx.size.y / 2, bx.size.y / 2),
+            Random.Range(-bx.size.z / 2, bx.size.z / 2));
+
+        currentTarget = (GameObject)Instantiate(Target, posSpawn, Quaternion.identity);
+        currentChild++;
+    }
+
+    void Start () {
+        StartChallenge();
+    }
+	
+	void Update () {
+	    if(currentTarget != null)
+        {
+            TargetScript ts = currentTarget.GetComponent<TargetScript>();
+            if(ts.dead)
+            {
+                Destroy(currentTarget);
+                spawnTarget();
+            }
+        }
+	}
+}
---
+++ VR_Simulation_Environment/Assets/Challenge/Target Range/TargetScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class TargetScript : MonoBehaviour, IGvrGazeResponder
+{
+    private bool            selected;
+    private float           currentTime = 0;
+    private MeshRenderer    meshRenderer;
+    private SphereCollider  sphereCollider;
+
+    [SerializeField]
+    private ParticleSystem  ParticleSystem;
+
+    private float           STARE_TIME = 0.3f;
+
+    public bool             dead { get; private set; }
+
+    void Start () {
+        meshRenderer = GetComponent<MeshRenderer>();
+        sphereCollider = GetComponent<SphereCollider>();
+    }
+	
+	void Update () {
+        if (selected)
+        {
+            currentTime += Time.deltaTime;
+            if(currentTime >= STARE_TIME)
+            {
+                ParticleSystem.transform.position = transform.position;
+                ParticleSystem.Play();
+                meshRenderer.enabled = false;
+                sphereCollider.enabled = false;
+                dead = true;
+            }
+        }
+	}
+
+    public void OnGazeEnter()
+    {
+        selected = true;
+        Console.WriteLine("START()");
+    }
+
+    public void OnGazeExit()
+    {
+        selected = false;
+        currentTime = 0;
+    }
+
+    public void OnGazeTrigger()
+    {
+        Console.WriteLine("OnGazeTrigger()");
+    }
+}
