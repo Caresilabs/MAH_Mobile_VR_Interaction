f014f65eb1b67eaa0095bfd910bbc9b9ff6fd825
Patrik Nilsson <craftopia@live.com>
2017-02-21 20:30:22 +0100

    Fixed circular menu + model

---
+++ VR_Simulation_Environment/Assets/Challenge/Hammer/CircleMenuScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class CircleMenuScript : MonoBehaviour {
+
+    [SerializeField]
+    private Player player;
+
+    [SerializeField]
+    private GameObject Canvas;
+
+    bool showing;
+
+    public void Enter(int type)
+    {
+        print("Callled " + type);
+        if (type == 1)
+        {
+            player.setHammer(true);
+            Destroy(transform.parent.gameObject);
+        }
+        else
+        {
+            Canvas.gameObject.SetActive(false);
+        }
+        Canvas.gameObject.SetActive(false);
+        showing = false;
+    }
+
+    public void Show()
+    {
+        if (!showing)
+        {
+            Canvas.gameObject.SetActive(true);
+            Quaternion rotation = Quaternion.LookRotation(transform.position - player.transform.position);
+            Canvas.transform.rotation = rotation;
+            showing = true;
+        }
+    }
+	void Start () {
+        Canvas.gameObject.SetActive(false);
+    }
+	
+	void Update () {
+    }
+}
